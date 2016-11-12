ac31912f5773e6d6d1913f8bf8086569e9d3e5c7
ReenExe <ipourri@gmail.com>
2016-11-13 01:45:27 +0200

    code before testing

--- player.js
+++ player.js
@@ -1,3 +1,45 @@
-module.exports = function () {
-  return [5, 5, 5];
+module.exports = function (matrix) {
+    var self = '11011011011111111111111111111111';
+
+    var around = [
+        0,      1,      2,
+        3,   /* i */    5,
+        6,      7,      8
+    ];
+
+    var allFree = true;
+    var freeIndex = [];
+    var selfIndex = [];
+    var enemyIndex = [];
+
+    for (var index of around) {
+        if (matrix[index] === 0) {
+            freeIndex.push(index);
+        } else if (matrix[index] === self) {
+            selfIndex.push(index);
+        } else {
+            enemyIndex.push(index);
+            allFree = false;
+        }
+    }
+
+    if (allFree) {
+        return [5, 5, 5, self];
+    }
+
+    if (enemyIndex.length === 0) {
+        if (freeIndex.length) {
+            var point = freeIndex[0];
+
+            return [point, point, point, self];
+        }
+
+        var point = selfIndex[Date.now() % selfIndex.length + 1];
+
+        return [point, point, point, self];
+    }
+
+    var point = enemyIndex[Date.now() % enemyIndex.length + 1];
+
+    return [point, point, point, self];
 };
--- test.js
+++ test.js
@@ -4,5 +4,13 @@ var player = require('./player');
 test('first test', function (t) {
     t.plan(1);
 
-    t.deepEqual([5, 5, 5], player([]));
+    var matrix = [
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0
+    ];
+
+    var self = '11011011011111111111111111111111';
+
+    t.deepEqual([5, 5, 5, self], player(matrix));
 });
